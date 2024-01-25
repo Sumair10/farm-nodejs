c986124c347a50ae82c01a895fb1d7238ffbd6c5
Sumair10 <sumairzahid123@gmail.com>
2024-01-25 15:41:17 +0400

    limiting fields

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -22,10 +22,19 @@ exports.getAllTours = async (req , res)=>{
 
         if(req.query.sort){
             const sortBy = req.query.sort.split(',').join(' ')
-            query= query.sort(sortBy)
+            query= query.sort(sortBy )
         }
         else{
             query = query.sort('createdAt')
+        }
+
+        if(req.query.fields){
+            const fields = req.query.fields.split(',').join(' ')
+            console.log(fields)
+            query = query.select(fields)
+        }
+        else{
+            query = query.select('-__v')
         }
 
         // const query = Tour.find()
