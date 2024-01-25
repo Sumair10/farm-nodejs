224ca702aececaab61766429344d1935d796c034
Sumair10 <sumairzahid123@gmail.com>
2024-01-25 16:13:48 +0400

    pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -37,11 +37,18 @@ exports.getAllTours = async (req , res)=>{
             query = query.select('-__v')
         }
 
-        // const query = Tour.find()
-        // .where('duration')
-        // .equals(5)
-        // .where('difficulty')
-        // .equals('easy')
+        const page = req.query.page * 1 || 1;
+        const limit = req.query.limit * 1 || 100;
+        const skip = (page - 1) * limit;
+        
+        query = query.skip(skip).limit(limit);
+        
+        if (req.query.page) {
+            const numTours = await Tour.countDocuments();
+            if (skip >= numTours) {
+                throw new Error("This page doesn't exist");
+            }
+        }
 
         const tours = await query
 
@@ -51,6 +58,7 @@ exports.getAllTours = async (req , res)=>{
             data :{tours}
         })
     } catch (error) {
+        console.log(error)
         res.status(400).json({
             status :'fail',
             message :"Invalid data received!"
